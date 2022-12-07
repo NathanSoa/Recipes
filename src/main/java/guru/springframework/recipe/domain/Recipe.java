c6e9255c90f6ada47021a2740ddb46bfd72b3919
NathanSoa <nathansoares809@gmail.com>
2022-12-07 19:35:57 -0300

    feat: add ingredient entity

--- src/main/java/guru/springframework/recipe/domain/Recipe.java
+++ src/main/java/guru/springframework/recipe/domain/Recipe.java
@@ -1,6 +1,7 @@
 package guru.springframework.recipe.domain;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class Recipe {
@@ -15,12 +16,12 @@ public class Recipe {
     private  String source;
     private String url;
     private String directions;
-
     @Lob
     private Byte[] image;
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
-
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
     public Long getId() {
         return id;
     }
@@ -99,5 +100,13 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
     }
 }
